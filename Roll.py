2a6fd07641b4e0b0735a767532261118035640d5
MatejGladis <48381972+MatejGladis@users.noreply.github.com>
2020-05-07 00:53:22 +0200

    add comments

--- Roll.py
+++ Roll.py
@@ -1,5 +1,7 @@
+#Created by Matko
 import random
 
+#Function IsValid checks if the entered input is number
 def IsValid(Expression):
     try:
         int(Expression)
@@ -7,48 +9,60 @@ def IsValid(Expression):
     except:
         return False
 
+#Function AddSpaces isolate numbers and characters form each other 
 def AddSpaces(Expression):
+    
+    #SpaceRamover creates ramoves spaces for the Expression in for i cyclus
     SpaceRamover = Expression.split(" ")
     Expression = ""
 
     for i in range(len(SpaceRamover)):
         Expression += SpaceRamover[i]
+    
     Position = 0
-
+    
+    #While isolate numbers and characters form each other 
     while True:
+        #This while works while it find first nonnumber character
         while True:
             Position += 1
             if Position >= len(Expression):
                 break
             if IsValid(Expression[(Position - 1):(Position + 1)]) == False:
                 break
-
+        
         if Position >= len(Expression):
             break
-
+        
+        #This adds space between characters
         Expression = Expression[:Position] + " " + Expression[(Position):]
         Position += 1
 
     return Expression
 
+#random roll
 def Roll(a):
     sum = random.randint(1, a)
     return sum
 
+#Write each roll and final sum of rolls
 def Calc(Expression):
     ExpressionList = Expression.split(" ")
     Positon = 0
     FinalList = []
     FinalSum = 0
-
+    
     while True:
 
         if IsValid(ExpressionList[Positon]) == True:
-
+            
+            #for + or -
             if len(ExpressionList) == Positon + 1 or IsValid(ExpressionList[Positon + 1]) == True:
 
                 FinalList.append(ExpressionList[Positon])
                 FinalSum += int(ExpressionList[Positon])
+                
+            #for dices   
             elif ExpressionList[Positon + 1] == "d" or ExpressionList[Positon + 1] == "D" and IsValid(ExpressionList[Positon + 2]) == False:
                 return False
             elif ExpressionList[Positon + 1] == "d" or ExpressionList[Positon + 1] == "D" and IsValid(ExpressionList[Positon + 2]) == True:
@@ -72,6 +86,7 @@ def Calc(Expression):
 
     return FinalList, FinalSum
 
+#Core of the program
 def Main(Expression):
     Expression = str(Expression)
     Expression = Expression[5:]
