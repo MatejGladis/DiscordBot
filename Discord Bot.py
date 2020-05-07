d2725bd7a6ac6446a85caa523ea6487291d1aee4
HeraldPunisher <64928253+HeraldPunisher@users.noreply.github.com>
2020-05-07 09:15:39 +0200

    Add files via upload

--- Discord Bot.py
+++ Discord Bot.py
@@ -1,13 +1,8 @@
 import random
 import discord
+import Bot
 from discord.ext import commands
 
-def IsValid(Expression):
-    try:
-        int(Expression)
-        return True
-    except:
-        return False
 
 client = commands.Bot(command_prefix="/")
 
@@ -20,18 +15,44 @@ async def ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")     
 
 @client.command(aliases = ["r"])
-async def roll(dices):
+async def roll(ctx, *, dices):
+    f = 0
+    for i in range (len(dices)):
+        d = "d"
+        w = dices[i]
+        if w == d:
+            f += 1
+    if f >= 2:
+        await ctx.send("Nauč sa hádzať kockami, prečo tam máš dve d?")
     a = dices.split("d")
     x = a[0]
-    if IsValid(x) == False:
+    if x == "":
         x = 1
+    else:
+        x = int(a[0])
     y = int(a[1])
-    b = []
+    b = ["Takéto hody si mal: "]
+    c = 0
     for i in range(x):
         z = random.randint(1, y)
+        c += z
         b.append(str(z))
-    await ctx.send(b)
+    e =""
+    for i in range(len(b)):
+        if b[i] == "20" and x == 20:
+            e += "***__"
+        if b[i] == "1":
+            e += "***__"
+        e += b[i]
+        if b[i] == "20" and x == 20:
+            e += "__***"
+        if b[i] == "1":
+            e += "__***"
+        e += "  "
+    e += "\nIch súčet je: "
+    e += str(c)
+    await ctx.send(e)
     
 
 
-client.run("NzA3NjkzNDYzOTM5OTczMTYy.XrMg9w.Wahl4ZINuAHTk_sh9xN491GrKJE")
+client.run("NzA3NjkzNDYzOTM5OTczMTYy.XrNIeQ.RN40fBfs28vs4eNuz6Jwo2K7wnw")
